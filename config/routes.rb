96fc928823d797f17237a46716fcc12927e54efb
Cody Poll <cpoll@instructure.com>
2015-09-19 22:55:48 -0600

    Initial setup junk

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   root controller: :static, action: :landing
 
   # The priority is based upon order of creation: first created -> highest priority.
