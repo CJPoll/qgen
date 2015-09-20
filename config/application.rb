82f3a2304de0e90fbb4f9c05f57830dd13bf379d
Cody Poll <cpoll@instructure.com>
2015-09-19 22:53:56 -0600

    Removed unnecessary config line

--- config/application.rb
+++ config/application.rb
@@ -31,6 +31,5 @@ module Qgen
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-    config.react.component_filenames = ['bundle.js']
   end
 end
