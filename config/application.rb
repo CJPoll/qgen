f42c6785ec7422c1c7f089e66948215c6afe31c0
Cody Poll <cpoll@instructure.com>
2015-09-19 19:52:17 -0600

    Configured react to look at bundle.js

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,6 @@ module Qgen
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.react.component_filenames = ['bundle.js']
   end
 end
