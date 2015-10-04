2526387d52477ffa8df66d4617a6bd39ee8cc71b
Cody Poll <cpoll@instructure.com>
2015-10-04 00:39:21 -0600

    Fixed the backstory entry bug

--- app/javascripts/components/backstoryEntry.jsx
+++ app/javascripts/components/backstoryEntry.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Reflux from 'reflux';
 import { Link } from 'react-router';
 import BackstoryActions from '../actions/backstoryActions';
 import BackstoryStore from '../stores/backstoryStore';
@@ -9,10 +8,14 @@ import FormGroup from './formGroup';
 var BackstoryEntry;
 
 BackstoryEntry = React.createClass({
-	mixins: [Reflux.connect(BackstoryStore, 'backstory')],
+	componentWillMount() {
+		var state = BackstoryStore.getInitialState();
+		this.setState({backstory: state.backstory});
+	},
 
 	handleKeyUp(e) {
 		BackstoryActions.setBackstory(e.target.value);
+		this.setState({backstory: e.target.value});
 	},
 
 	render() {
@@ -25,7 +28,7 @@ BackstoryEntry = React.createClass({
 				<PanelBody>
 					<p> Enter whatever backstory you would like for your character to have </p>
 					<FormGroup>
-						<textarea onChange={this.handleKeyUp} onKeyUp={this.handleKeyUp} value={this.state.backstory.backstory} className='textarea'/>
+						<textarea onChange={this.handleKeyUp} value={this.state.backstory} className='textarea'/>
 					</FormGroup>
 					<FormGroup>
 						<Link to='/characters/new/confirm' className='submit'> Move On </Link>
