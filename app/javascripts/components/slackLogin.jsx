8887329dfa414bc6273743e9daab51adb78763bf
Cody Poll <cpoll@instructure.com>
2015-10-15 00:12:43 -0600

    Should fix slack stuff - whoops

--- app/javascripts/components/slackLogin.jsx
+++ app/javascripts/components/slackLogin.jsx
@@ -6,7 +6,7 @@ var SlackLogin;
 SlackLogin = React.createClass({
 	render() {
 		return (
-			<a href="https://slack.com/oauth/authorize?scope=incoming-webhook&client_id=2528280221.12537519110">
+			<a href="/users/auth/slack">
 				<img alt="Add to Slack" height="40" width="139" src="https://platform.slack-edge.com/img/add_to_slack.png" srcset="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x"/>
 			</a>
 		);
